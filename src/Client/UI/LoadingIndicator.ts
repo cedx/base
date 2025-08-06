baa15c5d089dbde6d6883b4d39d5b764babc8d3c
Cédric Belin <cedx@outlook.com>
2025-08-06 23:37:26 +0200

    Add the `LoadingIndicator` component

---
+++ src/Client/UI/LoadingIndicator.ts
@@ -0,0 +1,58 @@
+import type {ILoadingIndicator} from "#Abstractions/ILoadingIndicator.js";
+import {html, type TemplateResult} from "lit";
+import {customElement} from "lit/decorators.js";
+import {Component} from "./Component.js";
+
+/**
+ * A component that shows up when an HTTP request starts, and hides when all concurrent HTTP requests are completed.
+ */
+@customElement("loading-indicator")
+export class LoadingIndicator extends Component implements ILoadingIndicator {
+
+	/**
+	 * The number of concurrent HTTP requests.
+	 */
+	#requestCount = 0;
+
+	/**
+	 * Creates a new loading indicator.
+	 */
+	constructor() {
+		super({shadowRoot: true});
+		this.hidden = true;
+	}
+
+	/**
+	 * Starts the loading indicator.
+	 */
+	start(): void {
+		this.#requestCount++;
+		this.hidden = false;
+		document.body.classList.add("loading");
+	}
+
+	/**
+	 * Stops the loading indicator.
+	 * @param options Value indicating whether to force the loading indicator to stop.
+	 */
+	stop(options: {force?: boolean} = {}): void {
+		this.#requestCount--;
+		if (options.force || this.#requestCount <= 0) {
+			this.#requestCount = 0;
+			this.hidden = true;
+			document.body.classList.remove("loading");
+		}
+	}
+
+	/**
+	 * Renders this component.
+	 * @returns The view template.
+	 */
+	protected override render(): TemplateResult {
+		return html`
+			<slot>
+				<div class="spinner-border text-light"></div>
+			</slot>
+		`;
+	}
+}
