34b6bf323f8e3240277bc1ef45e413c051da184a
Cédric Belin <cedx@outlook.com>
2025-08-06 23:48:45 +0200

    Add the `Http` module

---
+++ src/Client/Http/HttpClient.ts
@@ -0,0 +1,127 @@
+import type {ILoadingIndicator} from "#Abstractions/ILoadingIndicator.js";
+import {HttpError} from "./HttpError.js";
+
+/**
+ * Performs HTTP requests.
+ */
+export class HttpClient {
+
+	/**
+	 * The base URL of the remote service.
+	 */
+	readonly baseUrl: URL;
+
+	/**
+	 * The function returning the component used as loading indicator.
+	 */
+	readonly #loadingIndicator: () => ILoadingIndicator|null;
+
+	/**
+	 * Creates a new HTTP client.
+	 * @param options An object providing values to initialize this instance.
+	 */
+	constructor(options: HttpClientOptions = {}) {
+		const url = options.baseUrl ? (options.baseUrl instanceof URL ? options.baseUrl.href : options.baseUrl) : document.baseURI;
+		this.baseUrl = new URL(url.endsWith("/") ? url : `${url}/`);
+		this.#loadingIndicator = options.loadingIndicator ?? (() => document.body.querySelector("loading-indicator") as ILoadingIndicator|null);
+	}
+
+	/**
+	 * Performs a DELETE request.
+	 * @param url The URL of the resource to fetch.
+	 * @param options The request options.
+	 * @returns The server response.
+	 */
+	delete(url?: string|URL, options?: RequestInit): Promise<Response> {
+		return this.#fetch("DELETE", url, null, options);
+	}
+
+	/**
+	 * Performs a GET request.
+	 * @param url The URL of the resource to fetch.
+	 * @param options The request options.
+	 * @returns The server response.
+	 */
+	get(url?: string|URL, options?: RequestInit): Promise<Response> {
+		return this.#fetch("GET", url, null, options);
+	}
+
+	/**
+	 * Performs a PATCH request.
+	 * @param url The URL of the resource to fetch.
+	 * @param body The request body.
+	 * @param options The request options.
+	 * @returns The server response.
+	 */
+	patch(url?: string|URL, body?: unknown, options?: RequestInit): Promise<Response> {
+		return this.#fetch("PATCH", url, body, options);
+	}
+
+	/**
+	 * Performs a POST request.
+	 * @param url The URL of the resource to fetch.
+	 * @param body The request body.
+	 * @param options The request options.
+	 * @returns The server response.
+	 */
+	post(url?: string|URL, body?: unknown, options?: RequestInit): Promise<Response> {
+		return this.#fetch("POST", url, body, options);
+	}
+
+	/**
+	 * Performs a PUT request.
+	 * @param url The URL of the resource to fetch.
+	 * @param body The request body.
+	 * @param options The request options.
+	 * @returns The server response.
+	 */
+	put(url?: string|URL, body?: unknown, options?: RequestInit): Promise<Response> {
+		return this.#fetch("PUT", url, body, options);
+	}
+
+	/**
+	 * Performs a custom HTTP request.
+	 * @param method The HTTP method.
+	 * @param url The URL of the resource to fetch.
+	 * @param body The request body.
+	 * @param options The request options.
+	 * @returns The server response.
+	 */
+	async #fetch(method: string, url: string|URL = "", body: unknown = null, options: RequestInit = {}): Promise<Response> {
+		const headers = new Headers(options.headers);
+		if (!headers.has("accept")) headers.set("accept", "application/json");
+
+		if (body && !(body instanceof Blob || body instanceof FormData || body instanceof URLSearchParams)) {
+			if (typeof body != "string") body = JSON.stringify(body);
+			if (!headers.has("content-type")) headers.set("content-type", "application/json");
+		}
+
+		const loadingIndicator = this.#loadingIndicator();
+		try {
+			loadingIndicator?.start();
+			const request = new Request(new URL(url, this.baseUrl), {...options, method, headers, body} as RequestInit);
+			const response = await fetch(request);
+			if (!response.ok) throw new HttpError(response);
+			return response;
+		}
+		finally {
+			loadingIndicator?.stop();
+		}
+	}
+}
+
+/**
+ * Defines the options of a {@link HttpClient} instance.
+ */
+export type HttpClientOptions = Partial<{
+
+	/**
+	 * The base URL of the remote service.
+	 */
+	baseUrl: string|URL;
+
+	/**
+	 * The function returning the component used as loading indicator.
+	 */
+	loadingIndicator: () => ILoadingIndicator|null;
+}>;
---
+++ src/Client/Http/StatusCodes.ts
@@ -0,0 +1,140 @@
+/**
+ * Provides common HTTP status codes.
+ */
+export const StatusCodes = Object.freeze({
+
+	/**
+	 * The `OK` status.
+	 */
+	OK: 200,
+
+	/**
+	 * The `Created` status.
+	 */
+	Created: 201,
+
+	/**
+	 * The `No Content` status.
+	 */
+	NoContent: 204,
+
+	/**
+	 * The `Moved Permanently` status.
+	 */
+	MovedPermanently: 301,
+
+	/**
+	 * The `Found` status.
+	 */
+	Found: 302,
+
+	/**
+	 * The `Not Modified` status.
+	 */
+	NotModified: 304,
+
+	/**
+	 * The `Bad Request` status.
+	 */
+	BadRequest: 400,
+
+	/**
+	 * The `Unauthorized` status.
+	 */
+	Unauthorized: 401,
+
+	/**
+	 * The `Payment Required` status.
+	 */
+	PaymentRequired: 402,
+
+	/**
+	 * The `Forbidden` status.
+	 */
+	Forbidden: 403,
+
+	/**
+	 * The `Not Found` status.
+	 */
+	NotFound: 404,
+
+	/**
+	 * The `Method Not Allowed` status.
+	 */
+	MethodNotAllowed: 405,
+
+	/**
+	 * The `Not Acceptable` status.
+	 */
+	NotAcceptable: 406,
+
+	/**
+	 * The `Request Timeout` status.
+	 */
+	RequestTimeout: 408,
+
+	/**
+	 * The `Conflict` status.
+	 */
+	Conflict: 409,
+
+	/**
+	 * The `Payload Too Large` status.
+	 */
+	PayloadTooLarge: 413,
+
+	/**
+	 * The `Unsupported Media Type` status.
+	 */
+	UnsupportedMediaType: 415,
+
+	/**
+	 * The `Page Expired` status.
+	 */
+	PageExpired: 419,
+
+	/**
+	 * The `Unprocessable Content` status.
+	 */
+	UnprocessableContent: 422,
+
+	/**
+	 * The `Too Many Requests` status.
+	 */
+	TooManyRequests: 429,
+
+	/**
+	 * The `Internal Server Error` status.
+	 */
+	InternalServerError: 500,
+
+	/**
+	 * The `Not Implemented` status.
+	 */
+	NotImplemented: 501,
+
+	/**
+	 * The `Bad Gateway` status.
+	 */
+	BadGateway: 502,
+
+	/**
+	 * The `Service Unavailable` status.
+	 */
+	ServiceUnavailable: 503,
+
+	/**
+	 * The `Gateway Timeout` status.
+	 */
+	GatewayTimeout: 504,
+
+	/**
+	 * The `Bandwidth Limit Exceeded` status
+	 */
+	BandwidthLimitExceeded: 509
+});
+
+/**
+ * Provides common HTTP status codes.
+ */
+export type StatusCodes = typeof StatusCodes[keyof typeof StatusCodes];
