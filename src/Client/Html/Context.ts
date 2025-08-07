968548648c0a7ab63fd30409b3c114c316d1a74f
Cédric Belin <cedx@outlook.com>
2025-08-07 23:26:24 +0200

    Add the `Html.Context` enumeration

---
+++ src/Client/Html/Context.ts
@@ -0,0 +1,44 @@
+/**
+ * Defines contextual modifiers.
+ */
+export const Context = Object.freeze({
+
+	/**
+	 * A danger.
+	 */
+	Danger: "danger",
+
+	/**
+	 * A warning.
+	 */
+	Warning: "warning",
+
+	/**
+	 * An information.
+	 */
+	Info: "info",
+
+	/**
+	 * A success.
+	 */
+	Success: "success"
+});
+
+/**
+ * Defines contextual modifiers.
+ */
+export type Context = typeof Context[keyof typeof Context];
+
+/**
+ * Gets the icon corresponding to the specified context.
+ * @param context The context.
+ * @returns The icon corresponding to the specified context.
+ */
+export function contextIcon(context: Context): string {
+	switch (context) {
+		case Context.Danger: return "error";
+		case Context.Success: return "check_circle";
+		case Context.Warning: return "warning";
+		default: return "info";
+	}
+}
---
+++ src/Server/Html/Context.cs
@@ -0,0 +1,27 @@
+namespace Belin.Base.Html;
+
+/// <summary>
+/// Defines contextual modifiers.
+/// </summary>
+public enum Context {
+
+	/// <summary>
+	/// A danger.
+	/// </summary>
+	Danger,
+
+	/// <summary>
+	/// A warning.
+	/// </summary>
+	Warning,
+
+	/// <summary>
+	/// An information.
+	/// </summary>
+	Info,
+
+	/// <summary>
+	/// A success.
+	/// </summary>
+	Success
+}
