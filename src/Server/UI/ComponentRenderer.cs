c6e0b3fe3a0dd3535c367dfc1a32c9a396afa6ec
Cédric Belin <cedx@outlook.com>
2025-08-09 12:25:35 +0200

    Code optimization [skip ci]

--- src/Server/UI/ComponentRenderer.cs
+++ src/Server/UI/ComponentRenderer.cs
@@ -18,7 +18,7 @@ public class ComponentRenderer(IServiceProvider serviceProvider) {
 	/// <param name="model">The component model.</param>
 	/// <returns>The HTML representation of the component.</returns>
 	public async Task<string> ToHtml<T>(object? model = null) where T: IComponent =>
-		await ToHtml<T>(new Dictionary<string, object?> { ["Model"] = model });
+		await ToHtml<T>(model is null ? [] : new Dictionary<string, object?> { ["Model"] = model });
 
 	/// <summary>
 	/// Renders a component in HTML format.
@@ -29,8 +29,9 @@ public class ComponentRenderer(IServiceProvider serviceProvider) {
 	public async Task<string> ToHtml<T>(IDictionary<string, object?> parameters) where T: IComponent {
 		using var scope = serviceProvider.CreateScope();
 		await using var htmlRenderer = new HtmlRenderer(scope.ServiceProvider, serviceProvider.GetRequiredService<ILoggerFactory>());
-		return await htmlRenderer.Dispatcher.InvokeAsync(async () =>
-			(await htmlRenderer.RenderComponentAsync<T>(ParameterView.FromDictionary(parameters))).ToHtmlString()
-		);
+		return await htmlRenderer.Dispatcher.InvokeAsync(async () => {
+			var parameterView = parameters.Count == 0 ? ParameterView.Empty : ParameterView.FromDictionary(parameters);
+			return (await htmlRenderer.RenderComponentAsync<T>(parameterView)).ToHtmlString();
+		});
 	}
 }
