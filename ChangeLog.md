877a1f836d93d7a8aa35be0a9ecc5999b5d08ee3
Cédric Belin <cedx@outlook.com>
2025-08-10 02:50:50 +0200

    Add the `UI.Component` class [skip ci]

--- ChangeLog.md
+++ ChangeLog.md
@@ -2,6 +2,7 @@
 
 ## Version [0.6.0](https://github.com/cedx/base/compare/v0.5.0...v0.6.0)
 - Added the `Data.Pagination` and `Data.PaginatedList` classes.
+- Added the `UI.Component` class.
 
 ## Version [0.5.0](https://github.com/cedx/base/compare/v0.4.1...v0.5.0)
 - Added the `Data.Sort` class.
---
+++ src/Client/UI/Component.ts
@@ -0,0 +1,34 @@
+import {LitElement, type CSSResultGroup} from "lit";
+
+/**
+ * The optional base class for custom elements.
+ */
+export abstract class Component extends LitElement {
+
+	/**
+	 * The component styles.
+	 */
+	static override styles: CSSResultGroup = [document.adoptedStyleSheets];
+
+	/**
+	 * Value indicating whether this component uses a shadow root.
+	 */
+	readonly #useShadowRoot: boolean;
+
+	/**
+	 * Creates a new custom element.
+	 * @param options Value indicating whether this component uses a shadow root.
+	 */
+	constructor(options: {shadowRoot?: boolean} = {}) {
+		super();
+		this.#useShadowRoot = options.shadowRoot ?? false;
+	}
+
+	/**
+	 * Returns the node into which this component should render.
+	 * @returns The node into which this component should render.
+	 */
+	protected override createRenderRoot(): DocumentFragment|HTMLElement {
+		return this.#useShadowRoot ? super.createRenderRoot() : this;
+	}
+}
