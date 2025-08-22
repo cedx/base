16f6654df7b01daf5d5f2d6e8d5d0896ff12ff65
Cédric Belin <cedx@outlook.com>
2025-08-23 01:00:18 +0200

    Code formatting [skip ci]

--- src/Client/UI/Components/Toast.ts
+++ src/Client/UI/Components/Toast.ts
@@ -140,18 +140,10 @@ export class Toast extends HTMLElement {
 	 */
 	attributeChangedCallback(attribute: string, oldValue: string|null, newValue: string|null): void {
 		if (newValue != oldValue) switch (attribute) {
-			case "caption":
-				this.#updateCaption(newValue ?? "");
-				break;
-			case "context":
-				this.#updateContext(Object.values(Context).includes(newValue as Context) ? newValue as Context : Context.Info);
-				break;
-			case "culture":
-				this.#formatter = new Intl.RelativeTimeFormat((newValue ?? "").trim() || navigator.language, {style: "long"});
-				break;
-			case "icon":
-				this.#updateIcon(newValue ?? "")
-				break;
+			case "caption": this.#updateCaption(newValue ?? ""); break;
+			case "context": this.#updateContext(Object.values(Context).includes(newValue as Context) ? newValue as Context : Context.Info); break;
+			case "culture": this.#formatter = new Intl.RelativeTimeFormat((newValue ?? "").trim() || navigator.language, {style: "long"}); break;
+			case "icon": this.#updateIcon(newValue ?? ""); break;
 			// No default
 		}
 	}
