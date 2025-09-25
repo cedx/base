f5ef2188c188f7bc11348d801ca66ae84ae8c3b0
Cédric Belin <cedx@outlook.com>
2025-09-25 15:55:34 +0200

    Add new tests

---
+++ test/Server/Data/Mapping/IPAddressTypeHandlerTest.cs
@@ -0,0 +1,41 @@
+namespace Belin.Base.Data.Mapping;
+
+using Microsoft.Data.Sqlite;
+using System.Net;
+
+/// <summary>
+/// Tests the features of the <see cref="IPAddressTypeHandler"/> class.
+/// </summary>
+[TestClass]
+public sealed class IPAddressTypeHandlerTest {
+
+	[TestMethod]
+	public void Parse() {
+		var typeHandler = new IPAddressTypeHandler();
+
+		// It should return `null` if the value is invalid.
+		IsNull(typeHandler.Parse(123));
+		IsNull(typeHandler.Parse(string.Empty));
+
+		// It should return an IP address if the value is valid.
+		var value = typeHandler.Parse("127.0.0.1");
+		IsNotNull(value);
+		AreEqual("127.0.0.1", value.ToString());
+	}
+
+	[TestMethod]
+	public void SetValue() {
+		var parameter = new SqliteParameter();
+
+		// It should set the parameter to `null` if the value is `null`.
+		new IPAddressTypeHandler().SetValue(parameter, null);
+		IsNull(parameter.Value);
+
+		// It should set the parameter to the string representation if the value is not `null`.
+		new IPAddressTypeHandler(mapToIPv6: false).SetValue(parameter, IPAddress.Parse("127.0.0.1"));
+		AreEqual("127.0.0.1", parameter.Value);
+
+		new IPAddressTypeHandler(mapToIPv6: true).SetValue(parameter, IPAddress.Parse("127.0.0.1"));
+		AreEqual("::ffff:127.0.0.1", parameter.Value);
+	}
+}
---
+++ test/Server/Data/Mapping/PhysicalAddressTypeHandlerTest.cs
@@ -0,0 +1,39 @@
+namespace Belin.Base.Data.Mapping;
+
+using Microsoft.Data.Sqlite;
+using System.Net.NetworkInformation;
+
+/// <summary>
+/// Tests the features of the <see cref="PhysicalAddressTypeHandler"/> class.
+/// </summary>
+[TestClass]
+public sealed class PhysicalAddressTypeHandlerTest {
+
+	[TestMethod]
+	public void Parse() {
+		var typeHandler = new PhysicalAddressTypeHandler();
+
+		// It should return `null` if the value is invalid.
+		IsNull(typeHandler.Parse(123));
+		IsNull(typeHandler.Parse(string.Empty));
+
+		// It should return a physical address if the value is valid.
+		var value = typeHandler.Parse("8C:F8:C5:DE:C2:E0");
+		IsNotNull(value);
+		AreEqual("8CF8C5DEC2E0", value.ToString());
+	}
+
+	[TestMethod]
+	public void SetValue() {
+		var parameter = new SqliteParameter();
+		var typeHandler = new PhysicalAddressTypeHandler();
+
+		// It should set the parameter to `null` if the value is `null`.
+		typeHandler.SetValue(parameter, null);
+		IsNull(parameter.Value);
+
+		// It should set the parameter to the string representation if the value is not `null`.
+		typeHandler.SetValue(parameter, PhysicalAddress.Parse("8C:F8:C5:DE:C2:E0"));
+		AreEqual("8CF8C5DEC2E0", parameter.Value);
+	}
+}
