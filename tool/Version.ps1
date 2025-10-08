a8f7e0c3195b1b8a425e6e7bd322794531d8084b
Cédric Belin <cedx@outlook.com>
2025-10-08 19:24:51 +0200

    Optimisation de code

--- tool/Version.ps1
+++ tool/Version.ps1
@@ -1,6 +1,6 @@
 Write-Host "Updating the version number in the sources..."
 $version = Select-Xml "//Version" Package.xml
 (Get-Content "package.json") -replace '"version": "\d+(\.\d+){2}"', """version"": ""$version""" | Out-File "package.json"
-foreach ($item in Get-ChildItem "*.csproj" -Exclude "node_modules" -Recurse) {
+foreach ($item in Get-ChildItem "*/*.csproj" -Recurse) {
 	(Get-Content $item) -replace "<Version>\d+(\.\d+){2}</Version>", "<Version>$version</Version>" | Out-File $item
 }
