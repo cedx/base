64aba627da014e996a34496e91f88272aa1ad7b7
Cédric Belin <cedx@outlook.com>
2025-10-09 02:49:45 +0200

    Update the "Lint" script

--- tool/Lint.ps1
+++ tool/Lint.ps1
@@ -1,3 +1,4 @@
 Write-Output "Performing the static analysis of source code..."
+Invoke-ScriptAnalyzer $PSScriptRoot -ExcludeRule PSAvoidUsingPositionalParameters -Recurse
 npx tsc --build tsconfig.json --noEmit
 npx eslint --cache --cache-location=var --config=etc/ESLint.js src/Client test/Client
