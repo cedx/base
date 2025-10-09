ff28f83cf6718a6aa77c370c862e759d6d7cf551
Cédric Belin <cedx@outlook.com>
2025-10-09 11:15:16 +0200

    Code optimization

--- tool/Clean.ps1
+++ tool/Clean.ps1
@@ -1,5 +1,5 @@
 Write-Output "Deleting all generated files..."
 if (Test-Path "bin") { Remove-Item "bin" -Force -Recurse }
 if (Test-Path "lib") { Remove-Item "lib" -Force -Recurse }
-foreach ($item in Get-ChildItem "*/obj" -Recurse) { Remove-Item $item -Force -Recurse }
+Get-ChildItem "*/obj" -Recurse | ForEach-Object { Remove-Item $_ -Force -Recurse }
 Remove-Item "var/*" -Exclude ".gitkeep" -Force -Recurse
