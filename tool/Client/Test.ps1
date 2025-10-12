ec29e707e5cbb8d098b7710977db4bd1f248e64b
Cédric Belin <cedx@outlook.com>
2025-10-13 00:59:26 +0200

    Fix the CI

--- tool/Client/Test.ps1
+++ tool/Client/Test.ps1
@@ -1,4 +1,4 @@
 Write-Output "Running the client test suite..."
 npx tsc --build src/Client/tsconfig.json --sourceMap
-npx esbuild test/Client/Main.js --bundle --legal-comments=none --outfile=var/Tests.js
+npx esbuild test/Client/Main.js --bundle --legal-comments=none --outfile=var/Tests.js --sourcemap
 node test/Client/Playwright.js
