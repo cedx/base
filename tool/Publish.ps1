e4a09b2794b41dd13e3e2d06c955b4bf742ef9b0
Cédric Belin <cedx@outlook.com>
2025-10-23 17:33:12 +0200

    Code optimization [skip ci]

--- tool/Publish.ps1
+++ tool/Publish.ps1
@@ -16,7 +16,7 @@ npm publish --registry=https://registry.npmjs.org
 npm publish --registry=https://npm.pkg.github.com
 
 dotnet pack --output var
-foreach ($item in Get-ChildItem "var/*.nupkg") {
+foreach ($item in Get-Item "var/*.nupkg") {
 	dotnet nuget push $item --api-key $Env:NUGET_API_KEY --source https://api.nuget.org/v3/index.json
 	dotnet nuget push $item --api-key $Env:GITHUB_TOKEN --source https://nuget.pkg.github.com/cedx/index.json
 }
