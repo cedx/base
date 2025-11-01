e024fc74b991c781d24d796ba0a65985878d56dc
Cédric Belin <cedx@outlook.com>
2025-11-01 22:14:34 +0100

    Update the "Publish" script

--- tool/Publish.ps1
+++ tool/Publish.ps1
@@ -12,11 +12,9 @@ else {
 $version = (Import-PowerShellDataFile "Base.psd1").ModuleVersion
 git tag "v$version"
 git push origin "v$version"
-npm publish --registry=https://registry.npmjs.org
-npm publish --registry=https://npm.pkg.github.com
+npm publish
 
 dotnet pack --output var
 foreach ($item in Get-Item "var/*.nupkg") {
-	dotnet nuget push $item --api-key $Env:NUGET_API_KEY --source https://api.nuget.org/v3/index.json
-	dotnet nuget push $item --api-key $Env:GITHUB_TOKEN --source https://nuget.pkg.github.com/cedx/index.json
+	dotnet nuget push $item --api-key $Env:NUGET_API_KEY
 }
