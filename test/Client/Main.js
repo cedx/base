ed6ea9153cf719d68554d5064b90dd9802124455
Cédric Belin <cedx@outlook.com>
2026-01-11 12:41:56 +0100

    Reorganize the tests

--- test/Client/Main.js
+++ test/Client/Main.js
@@ -1,10 +1,9 @@
-import "./Base/DateRange.Tests.js";
-import "./Base/Date.Tests.js";
-import "./Base/File.Tests.js";
-import "./Base/Number.Tests.js";
-import "./Base/String.Tests.js";
-import "./Base/Data/Pagination.Tests.js";
-// import "./Base/Data/Sort.Tests.js";
-
-import "./Base.UI/Form.Tests.js";
-import "./Base.UI/Tag.Tests.js";
+import "./DateRange.Tests.js";
+import "./Date.Tests.js";
+import "./File.Tests.js";
+import "./Number.Tests.js";
+import "./String.Tests.js";
+import "./Data/Pagination.Tests.js";
+// import "./Data/Sort.Tests.js";
+import "./UI/Form.Tests.js";
+import "./UI/Tag.Tests.js";
