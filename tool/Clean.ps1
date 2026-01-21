e4bfc3ab3923c6dd396a7a9224b18662028a874e
Cédric Belin <cedx@outlook.com>
2026-01-21 10:01:43 +0100

    Code formatting

--- tool/Clean.ps1
+++ tool/Clean.ps1
@@ -1,4 +1,4 @@
 "Deleting all generated files..."
-"bin", "lib" | Remove-Item -ErrorAction Ignore -Force -Recurse
-Get-ChildItem "*/obj" -Recurse | Remove-Item -Force -Recurse
-Remove-Item "var/*" -Exclude ".gitkeep" -Force -Recurse
+Remove-Item "bin", "lib" -ErrorAction Ignore -Force -Recurse
+Get-ChildItem */obj -Recurse | Remove-Item -Force -Recurse
+Remove-Item var/* -Exclude .gitkeep -Force -Recurse
--- tool/Publish.ps1
+++ tool/Publish.ps1
@@ -9,12 +9,12 @@ else {
 }
 
 "Publishing the package..."
-$version = (Import-PowerShellDataFile "Base.psd1").ModuleVersion
+$version = Import-PowerShellDataFile Base.psd1 | Select-Object -ExpandProperty ModuleVersion
 git tag "v$version"
 git push origin "v$version"
 npm publish
 
 dotnet pack --output var
-foreach ($item in Get-Item "var/*.nupkg") {
-	dotnet nuget push $item --api-key $Env:NUGET_API_KEY --source https://api.nuget.org/v3/index.json
+Get-Item var/*.nupkg | ForEach-Object {
+	dotnet nuget push $_ --api-key $Env:NUGET_API_KEY --source https://api.nuget.org/v3/index.json
 }
