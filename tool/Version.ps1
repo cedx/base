265c37f16e8d1602487543e99459a8ede6d34219
Cédric Belin <cedx@outlook.com>
2026-02-05 12:28:23 +0100

    Code optimization [skip ci]

--- tool/Version.ps1
+++ tool/Version.ps1
@@ -1,6 +1,6 @@
 "Updating the version number in the sources..."
 $version = Import-PowerShellDataFile Base.psd1 | Select-Object -ExpandProperty ModuleVersion
-(Get-Content package.json) -replace '"version": "\d+(\.\d+){2}"', """version"": ""$version""" | Out-File package.json
+(Get-Content package.json -Raw) -replace '"version": "\d+(\.\d+){2}"', """version"": ""$version""" | Set-Content package.json -NoNewLine
 Get-ChildItem -Exclude node_modules -Filter *.csproj -Recurse | ForEach-Object {
-	(Get-Content $_) -replace "<Version>\d+(\.\d+){2}</Version>", "<Version>$version</Version>" | Out-File $_
+	(Get-Content $_ -Raw) -replace "<Version>\d+(\.\d+){2}</Version>", "<Version>$version</Version>" | Set-Content $_ -NoNewLine
 }
