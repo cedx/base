f70f1f3efdae697caa1524d75ba6bf5f4986cebd
Cédric Belin <cedx@outlook.com>
2026-02-19 22:55:00 +0100

    Code formatting

--- src/Server/Base/Data/PaginatedList.cs
+++ src/Server/Base/Data/PaginatedList.cs
@@ -9,13 +9,14 @@ public class PaginatedList<T>: List<T> {
 	/// <summary>
 	/// The information relevant to the pagination of the list items.
 	/// </summary>
-	public Pagination Pagination { get; set; } = new Pagination();
+	public Pagination Pagination { get; set; } = new();
 
 	/// <summary>
 	/// Creates an empty paginated list.
 	/// </summary>
 	/// <param name="itemsPerPage">The number of items per page.</param>
 	/// <returns>An empty paginated list with the specified number of items per page.</returns>
-	public static PaginatedList<T> Empty(int itemsPerPage) =>
-		new() { Pagination = new() { ItemsPerPage = itemsPerPage } };
+	public static PaginatedList<T> Empty(int itemsPerPage) => new() {
+		Pagination = new() { ItemsPerPage = itemsPerPage }
+	};
 }
